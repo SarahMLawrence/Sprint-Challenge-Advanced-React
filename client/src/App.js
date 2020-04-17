d05c68324834f7bec9c21eba00adc4670bc73de9
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-04-17 19:46:24 -0400

    taking out styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
+import './styles.scss';
 
 import Navbar from './components/Navbar';
 import PlayerCard from './components/PlayerCard';
