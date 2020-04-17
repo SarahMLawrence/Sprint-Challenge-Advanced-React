ce44425402030b6354040b15fe5f7e03948711a1
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-04-17 19:22:56 -0400

    inital commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "PlayerCard" from "./co"
+
+class App extends React.Component {
+
+  //constructor
+  constructor() {
+    super();
+    this.state = {
+      playerInfo: []
+    };
+  }
+
+  //did mount
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+      .then(res => res.json())
+      .then(playerData => {
+        console.log("Player:", playerData);
+        this.setState({ playerInfo: playerData });
+      })
+
+      .catch(err => {
+        console.log(err);
+      })
+
+
+  }
+
+  render() {
+
+    console.log("Rendering");
+
+
+    return (
+
+      <div>
+        <div className="playerCard">
+          <h3>{this.state.plyaerInfo.name}</h3>
+          <p>{this.state.playerInfo.country}</p>
+          <p>{this.state.playerInfo.searches}</p>
+
+        </div>
+
+      </div>
+      )
+
+  }
 }
 
 export default App;
