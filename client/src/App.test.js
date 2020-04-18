da4feedc36155e83b4dee7ece3fb407213607ef8
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-04-17 20:03:32 -0400

    testing the App

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import PlayerCard from './components/PlayerCard';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 
-it('displays name', () => {
+test('displays name', () => {
   const h3 = document.createElement('h3');
   ReactDOM.render(<PlayerCard />, h3);
   ReactDOM.unmountComponentAtNode(h3);
